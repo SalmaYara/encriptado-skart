c37e646635222058bbfd8c1436a521aa3155225c
SalmaYara <129403125+SalmaYara@users.noreply.github.com>
2024-03-02 01:40:26 -0600

    En proceso apps.js

--- apps.js
+++ apps.js
@@ -1,1 +1,17 @@
 console.log("Ejecutando el sistema")
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta-http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content-"width=devide-width,initial-scal
+    <title><h1>Encriptado de Skart</h1></title>
+  
+alert("Comunicado especial:);
+prompt("Se le indica que no debe usar letras mayusculas ni signos especiales");
+</head>
+<body>
+alert("Comunicado especial:);
+prompt("Se le indica que no debe usar letras mayusculas ni signos especiales");
+</body>
+</html>
